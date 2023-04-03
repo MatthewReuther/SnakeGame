7bef237ba0d214ebbe2b3c9b2ce2b46f6a8be2ea
Matthew Reuther <matthew.reuther@gmail.com>
2023-04-03 14:29:12 -0500

    implemented all fields needed

--- src/main/java/com/games/snakegame/GamePanel.java
+++ src/main/java/com/games/snakegame/GamePanel.java
@@ -13,6 +13,29 @@ public class GamePanel extends JPanel implements ActionListener {
     static final int SCREEN_WIDTH = 800;
     static final int SCREEN_HEIGHT = 800;
 
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT)/UNIT_SIZE;
+    //higher the slower game is
+    static final int DELAY = 75;
+
+    //holds coordinates for all body parts of our snake
+    final int xCords[]   = new int[GAME_UNITS];
+    final int yCords[]   = new int[GAME_UNITS];
+
+    //initial amount of snake body parts
+    int bodyParts = 6;
+
+    //Set score to amount of 'apples' eaten
+    int score;
+    int scoreX;
+    int scoreY;
+
+    //Set direction of snake at start
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
+
     GamePanel() {
 
     }
